7b4506c619f1aca0f196fdcdd0c418c28906e1f8
guodongAndroid <sun33919135@gmail.com>
2023-11-21 14:48:53 +0800

    Add TestRunner.kt

---
+++ kcp/plugin-kotlin/src/test/kotlin/com/guodong/android/sword/kcp/kotlin/TestRunner.kt
@@ -0,0 +1,24 @@
+package com.guodong.android.sword.kcp.kotlin
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+/**
+ * Created by guodongAndroid on 2023/11/21.
+ */
+@RunWith(Suite::class)
+@SuiteClasses(
+    TestAnonymousClass::class,
+    TestCollectionArg::class,
+    TestInnerClass::class,
+    TestLocalFunction::class,
+    TestNoArg::class,
+    TestPrimitiveArg::class,
+    TestPrimitiveArrayArg::class,
+    TestProxyDisabled::class,
+    TestReferenceArg::class,
+    TestTopLevelAndObject::class,
+)
+class TestRunner {
+}
